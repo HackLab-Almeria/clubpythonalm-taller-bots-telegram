4e8e7b59125b2ba4ffd1efce5c329d61d6fd18a8
HLComputilo <Kunmistero@users.noreply.github.com>
2016-05-04 14:35:02 +0200

    Update mitelegram.py

--- 04-MiTelegram/mitelegram.py
+++ 04-MiTelegram/mitelegram.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8-*-
+"""
+	Fichero para importar todos los datos de bot necesarios
+	Libreria: pyTelegramBotAPI 1.4.2 [ok]
+	Libreria: pyTelegramBotAPI 2.0 [ok]
+	Python: 3.5.1
+"""
 
 import telebot
 import sys
 
 TOKEN = 'AQUÍ EL NUMERO DE VUESTRO TOKEN entre comillas' 
 telegram = telebot.TeleBot(TOKEN) # Combinamos la declaración del Token con la función de la API
-chatID=0000 # Cuando ya  tengáis vuestro chatID. Siempre AQUÏ
+chatID=0 # Cuando ya  tengáis vuestro chatID. Siempre AQUÏ
 nombre_bot="Club_Python_Almeria"
 nombre_usuario="Club_Python_Almeria_bot"
 yo=" <- Tu Propia Alias -> " # Por si quieres usar un alias distinto al de Telegram
