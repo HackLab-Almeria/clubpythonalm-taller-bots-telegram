6e97ad81b4c6825232d3193ea66f45b756cfee8c
HLComputilo <Kunmistero@users.noreply.github.com>
2016-05-04 14:29:45 +0200

    Update micodigo.py

--- 04-MiTelegram/micodigo.py
+++ 04-MiTelegram/micodigo.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+	Libreria: pyTelegramBotAPI 1.4.2 [ok]
+	Libreria: pyTelegramBotAPI 2.0 [ok]
 	Este ejercicio muestra como importar todo el código del Token y el ChatID
 	sin tener que mostrarlo aquí. Y como enviar mensajes al bot desde cualquier
 	otro programa en Python que estemos desarrollando. 
 """
 
 from mitelegram import *
+
+yo="Pepito -> "
 
 mi_codigo="Este mensaje se leerá en el bot"
 
