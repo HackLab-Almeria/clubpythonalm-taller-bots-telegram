7046f02d44583ce63ac7266c9c0094ee2c28b339
HLComputilo <Kunmistero@users.noreply.github.com>
2016-05-04 14:48:21 +0200

    Update 06-Pegatinas.py

--- 00-Envios al Bot/06-Pegatinas.py
+++ 00-Envios al Bot/06-Pegatinas.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """ Ejemplo: 06 - Enviar Pegatinas (stickers) de Telegram
-  	Libreria: pyTelegramBotAPI 1.4.2
+  	Libreria: pyTelegramBotAPI 1.4.2 [ok]
+  	Libreria: pyTelegramBotAPI 2.0 [ok]
 	Python: 3.5.1
 """
 
@@ -10,9 +11,10 @@ import telebot
 TOKEN='AQUÍ EL NUMERO DE VUESTRO TOKEN' #  Identificador único del bot
 telegram = telebot.TeleBot(TOKEN) # Combinamos la declaración del Token con la función de la API
 'El chatID se obtiene al ejecutar el ejercicio 00-chatID.py'
-chatID=000 # <- Ese número de chatID se coloca aquí para todos los ejercicios
+chatID=0 # <- Ese número de chatID se coloca aquí para todos los ejercicios
 
 saludo="Hola Bot..."
+
 try:
 	telegram.get_me() # Comprobar que el bot está activo
 	print ("Conectando con el Bot de Telegram... [OK]")
@@ -24,7 +26,7 @@ try:
 	sys.exit(0)
 except Exception as e:
 	print ("Conectando con Bot de Telegram -> ERROR")
-	#print (e)
+	print (e) # Muestra mensaje del error producido en el bloque try
 	sys.exit(1)
 
 
